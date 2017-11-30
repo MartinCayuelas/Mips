69d6fadb2e6523d16fb4f005688d9e598330ea06
MartinCayuelas <martin.cayuelas@etu.umontpellier.fr>
2017-11-30 18:07:39 +0100

    update Pi.asm

--- PI.asm
+++ PI.asm
@@ -1,4 +1,9 @@
-# $s0 contient sqrt(2)/2
+# CAYUELAS Martin et MAYEUR Yannick
+
+# Pour des raisons d'optimisation nous avons fait en sorte que la fonction racine renvoit 0,0 pour sqrt(0) car elle renvoyait 0.0078125
+
+# Le calcul devient assez faux a partir du rang 127.
+
 # $s1 contient bn
 # $s2 contient b2n
 # $s3 contient Bn
@@ -12,9 +17,12 @@ eps:	.float 0.0001
 init:	.float 1.0
 two:	.float 2.0
 one:	.float 1.0
-
-texte: 	.asciiz "Entrez un entier n, correspondant au nombre de boucles\n"
+racine:	.float 0.7071067812
+zero:	.float 0.0
+texte: 	.asciiz "Entrez un entier x, correspondant au nombre de boucles effectuées\n"
 rl:	.asciiz "\n"
+espace: .asciiz "        "
+affichage: .asciiz "n:       Pi: \n"
 
 .text
 
@@ -24,17 +32,6 @@ main:	# initialisation
 	li $t2, 2 # n = 2 a la premiere iteration
 	l.s $f22, two
 	
-	# calcul de la constante sqrt(2)/2
-	l.s $f1, two
-	mfc1 $a0, $f1
-	jal sqrt
-	# ici $v0 vaut sqrt(2)
-	mtc1 $v0, $f2
-	div.s $f1, $f2, $f1
-	mfc1 $s0, $f1 # On a sqrt(2)/2
-	
-	
-
 	#affiche texte qui demande n
 	li $v0, 4
 	la $a0, texte
@@ -42,31 +39,67 @@ main:	# initialisation
 	li $v0, 5
 	syscall
 	
+	
 	move $t1, $v0
 	li $t0, 0 #cpt
+	move $a0, $s1
 	jal cb2n
+	move $s2, $v0
 	move $s1, $s2
+	
+	
+	
 	
 while:	bge $t0, $t1, end
 	li $t3, 2
 	l.s $f1, two
 	mul $t2, $t2, $t3 # n = 2n
-	mul.s $f22, $f22, $f1	
+	mul.s $f22, $f22, $f1
+	move $a0, $s1
 	jal cb2n
+	move $s2, $v0
+	move $a0, $s2
+	move $a1, $s1
 	jal cBn
+	move $s3, $v0
+	move $a0, $s1
 	jal cpn
-	jal cPn
+	move $s4, $v0
+	move $a0, $s3
+	jal cpn
+	move $s5, $v0
+	move $a0, $s4
+	move $a1, $s5
 	jal cmoy
+	move $s6, $v0
 	move $s1, $s2
-	addi $t0, $t0, 1 #Incremente le compteur
+	addi $t0, $t0, 1
+	
+	#Affichage de n
+	move $a0, $t2
+	li $v0, 1
+	syscall
+	
+	li $v0, 4
+	la $a0, espace
+	syscall
+	
+	#affichage de Pi
+	mtc1 $s6, $f12
+	li $v0, 2
+	syscall 
+	
+	li $v0, 4
+	la $a0, rl
+	syscall
 	j while
 	
-		
+# a0 = bn
 cb2n:	subi $sp, $sp, 4
 	sw $ra, 0($sp)
 	# calcul de bn^2
-	mtc1 $s1, $f1
-	mul.s $f0, $f1, $f1
+	mtc1 $a0, $f5
+	mul.s $f0, $f5, $f5
 	# calcul de sqrt(1 - bn²)
 	l.s $f1, one
 	sub.s $f2, $f1, $f0
@@ -80,18 +113,18 @@ cb2n:	subi $sp, $sp, 4
 	jal sqrt
 	# calcul de bn/(lereste)
 	mtc1 $v0, $f0
-	mtc1 $s1, $f4
-	div.s $f1, $f4, $f0
+	div.s $f1, $f5, $f0
 	# cacul de b2n
-	mtc1 $s0, $f0
+	l.s $f0, racine
 	mul.s $f2, $f0, $f1
-	mfc1 $s2, $f2
+	mfc1 $v0, $f2
 	lw $ra, 0($sp)
 	addi $sp, $sp, 4
 	jr $ra
-	
+
+# a0 = b2n, a1 = bn
 cBn:	# calcul de b2n²
-	mtc1 $s2, $f1
+	mtc1 $a0, $f1
 	mul.s $f3, $f1, $f1
 	# calcul de 2b2n²
 	l.s $f2, two
@@ -100,35 +133,33 @@ cBn:	# calcul de b2n²
 	l.s $f1, one
 	sub.s $f2, $f1, $f4
 	# calcul bn/reste
-	mtc1 $s1, $f1
+	mtc1 $a1, $f1
 	div.s $f3, $f1, $f2
-	mfc1 $s3, $f3
+	mfc1 $v0, $f3
 	jr $ra
 
-cpn:	mtc1 $s1, $f0
+# a0 = bn ou Bn, suivant le calcul à effectuer
+#Cette fonction est utilisée pour pn et Pn
+cpn:	mtc1 $a0, $f0
 	mul.s $f2, $f0, $f22
-	mfc1 $s4, $f2
-	jr $ra
-	
-cPn:	mtc1 $s3, $f0
-	mul.s $f2, $f0, $f22
-	mfc1 $s5, $f2
+	mfc1 $v0, $f2
 	jr $ra
 
-
+# a0 = pn, a1 = Pn, Calcul de la moyenne
 cmoy:	l.s $f2, two
-	mtc1 $s4, $f0
-	mtc1 $s5, $f1
+	mtc1 $a0, $f0
+	mtc1 $a1, $f1
 	add.s $f3, $f0, $f1
 	div.s $f1, $f3, $f2
-	mfc1 $s6, $f1
+	mfc1 $v0, $f1
 	jr $ra
 	
 
 
-# pour appeller sqrt il faut que la valeur n soit dans $a0 et il faut jal le res sera dans $v0
+# Pour appeller sqrt il faut que la valeur n soit dans $a0 et il faut jal. Le resultat sera dans $v0
 sqrt:	subi $sp, $sp, 4
 	sw $ra, 0($sp)
+	beqz $a0, fzero
 	l.s $f0, init
 	mtc1 $a0, $f1 
 	l.s $f4, eps
@@ -151,10 +182,13 @@ endsqrt:	mfc1 $v0, $f0
 		addi $sp, $sp, 4
 		jr $ra
 		
-end:	mtc1 $s6, $f12
-	li $v0, 2
-	syscall 
-	# Exits the program
+fzero:	l.s $f0, zero
+	mfc1 $v0, $f0
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
+		
+end:	# Exits the program
 	li $v0, 10
 	syscall
 	
